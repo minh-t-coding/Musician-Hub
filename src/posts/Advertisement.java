a9ab36c6d92b0e4f8142239b02fc71ba5ce957b8
Connor Musick <connormusick@Connors-MacBook-Pro.local>
2018-11-06 10:29:22 -0700

    updated advertisements to have genre and a link

--- src/posts/Advertisement.java
+++ src/posts/Advertisement.java
@@ -2,8 +2,27 @@ package posts;
 
 public class Advertisement extends Post{
 	private String link; //link to businesses/companies website
+	private String genre; //used to display ads based on similar genres
 	
 	public Advertisement() {
 		link = "";
+		genre = "";
 	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	
 }
