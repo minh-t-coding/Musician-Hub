495ef19b1356f75a4e742db9e50e36c61cc2edb8
Miranda <mirandahampton@email.arizona.edu>
2018-11-07 14:32:47 -0700

    Advertisements and MeetUps

--- src/users/Musician.java
+++ src/users/Musician.java
@@ -67,6 +67,52 @@ public class Musician extends SuperUser{
 		}
 		
 	}
+	
+	public void createMeetUp() throws FileNotFoundException {
+		MeetUp update = new MeetUp();
+		System.out.println("Enter the Date and Location of meetup seperated by comma(,): \n");
+		Scanner scanned = new Scanner (System.in);
+		String details = scanned.nextLine();
+		System.out.println("Enter the information for the MeetUp: \n");
+		scanned = new Scanner (System.in);
+		String status = scanned.nextLine();
+		update.setContent(status);
+		update.setOwner(this);
+		update.setDate(details.split(",")[0]);
+		update.setLocation(details.split(",")[1]);
+		File posts = new File("posts.txt");
+		BufferedReader bf = null;
+		FileWriter writer = null;
+		String oldContent ="";
+		try {
+			bf = new BufferedReader(new FileReader(posts));
+			String line = bf.readLine();
+			while(line!= null) {
+				oldContent = oldContent + line + System.lineSeparator();
+				line = bf.readLine();
+			}
+			String content = this.getUsername() + ":" + LocalDateTime.now() + ":meetup" + System.lineSeparator()
+					+"=================================================================="+ System.lineSeparator()
+					+ "Date: " + update.getDate() + " Location: " + update.getLocation() + System.lineSeparator()
+					+ status + System.lineSeparator()+ "==================================================================\n"
+					+ System.lineSeparator();
+			oldContent = oldContent + content;
+			writer = new FileWriter(posts);
+			writer.write(oldContent);
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				writer.close();
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
 
 	
 }
