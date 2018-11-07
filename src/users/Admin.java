fd0f796fe54ce22bb95b4401b4736dac2ab8caa6
Miranda <mirandahampton@email.arizona.edu>
2018-11-07 11:17:27 -0700

    take away admin

--- src/users/Admin.java
+++ src/users/Admin.java
@@ -48,10 +48,46 @@ public class Admin extends SuperUser {
 				}
 			}
 			System.out.println(old);
-			
+		}
+	
+	public void takeAwayAdmin(Admin admin) throws FileNotFoundException {
+		Musician mus = new Musician();
+		mus.setUsername(admin.getUsername());
+		mus.setPassword(admin.getPassword());
+		mus.setRealName(admin.getRealName());
+		File db = new File("Database.txt");
+		String old = "";
+		BufferedReader br = new BufferedReader(new FileReader(db));
+		try {
+			String line = br.readLine();
+			while (line != null) {
+				if(line.startsWith(admin.getUsername())) {
+					String newStr = line.replaceFirst("admin", "musician");
+					old = old + newStr+ System.lineSeparator();
+				}
+				
+				else {
+					old = old + line+ System.lineSeparator();
+				}
+				line = br.readLine();
+			}
 			
 		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				br.close();
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println(old);
+	}
 
+	
 	@Override
 	public ArrayList<SuperUser> memberLookup(String lookupUser) {
 		// TODO Auto-generated method stub
