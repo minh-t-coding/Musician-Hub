804059034ac81f3e2efa050fcca5046359fa1340
minh-t-coding <melnguyen115@gmail.com>
2018-11-07 09:35:39 -0700

    Return type to Create Account

--- src/program/Hub.java
+++ src/program/Hub.java
@@ -1,10 +1,16 @@
 package program;
 import java.util.*;
+
+import users.Admin;
+import users.Company;
+import users.Musician;
+import users.SuperUser;
+
 import java.io.*;
 
 public class Hub {
 	
-	public void signIn() throws FileNotFoundException {
+	public SuperUser signIn() throws FileNotFoundException {
 		System.out.println("Please enter your username: \n");
 		Scanner scannedUsername = new Scanner (System.in);
 		String username = scannedUsername.nextLine();
@@ -23,18 +29,22 @@ public class Hub {
 				String passw = line.split(",")[1].substring(0, line.split(",")[1].length() -1);
 				if(passw.toLowerCase().equals(password.toLowerCase())){
 					if(line.split(":{")[1].split(",")[0] == "musician") {
-						
+						Musician mus = new Musician(username,password);
+						return mus;
 					}
 					else if(line.split(":{")[1].split(",")[0] == "admin"){
-						
+						Admin adm = new Admin(username,password);
+						return adm;
 					}
 					else if(line.split(":{")[1].split(",")[0] == "company") {
-						
+						Company comp = new Company(username,password);
+						return comp;
 					}
 				}
 			}
 		}
 		System.out.println("Username does not exist.");
+		return new Musician("None","None");
 	}
 	public void createAccount() {
 		System.out.println("yooo x 2");
