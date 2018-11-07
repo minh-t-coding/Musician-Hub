ae36dc89d0f41f437522639708dfda440d0a9832
Connor Musick <connormusick@Connors-MacBook-Pro.local>
2018-11-06 20:39:24 -0700

    hub and driver updates

---
+++ src/program/Hub.java
@@ -0,0 +1,21 @@
+package program;
+import java.util.*;
+import java.io.*;
+
+public class Hub {
+	
+	public void signIn() {
+		System.out.println("Please enter your username: \n");
+		Scanner scannedUsername = new Scanner (System.in);
+		String username = scannedUsername.nextLine();
+		System.out.println("Please enter your password: \n");
+		Scanner scannedPassword = new Scanner (System.in);
+		String password = scannedUsername.nextLine();
+		
+		File database = new File("Database.txt");
+		
+	}
+	public void createAccount() {
+		System.out.println("yooo x 2");
+	}
+}
