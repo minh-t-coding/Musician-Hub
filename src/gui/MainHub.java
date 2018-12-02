8e8de294eb847f04b2c437362814d88a620033ca
Minh Nguyen <melnguyen115@gmail.com>
2018-12-01 18:47:08 -0700

    Profile Tab and functions

--- src/gui/MainHub.java
+++ src/gui/MainHub.java
@@ -10,9 +10,15 @@ import users.*;
 public class MainHub extends JFrame{
 	private Hub session;
 	private SuperUser signedIn;
+	
 	private JMenuBar menuBar;
+	
+	private JMenu profileOptions;
 	private JMenu signOut;
+	
 	private JMenuItem exit;
+	private JMenuItem changeInfo;
+	private JMenuItem showInfo;
 	
 	public MainHub(Hub hub, SuperUser signedInUser){
 		super("Musicians Hub");
@@ -25,8 +31,18 @@ public class MainHub extends JFrame{
 	}
 	public void buildGUI() {
 		signOut = new JMenu("Sign Out");
+		
+		profileOptions = new JMenu("Profile");
+		showInfo = new JMenuItem("Show Information");
+		changeInfo = new JMenuItem("Edit Information");
+		profileOptions.add(showInfo);
+		profileOptions.add(changeInfo);
+		showInfo.addActionListener(new MenuListener());
+		changeInfo.addActionListener(new MenuListener());
+		
 		menuBar = new JMenuBar();
-		signOut.addActionListener(new MenuListener());
+		//signOut.addActionListener(new MenuListener());
+		menuBar.add(profileOptions);
 		menuBar.add(signOut);
 		setJMenuBar(menuBar);
 	}
@@ -39,6 +55,44 @@ public class MainHub extends JFrame{
 				setVisible(false);
 				dispose();
 			}
+			if(source.equals(showInfo)) {
+				handleShowInfo();
+			}
+			if(source.equals(changeInfo)) {
+				handleChangeInfo();
+			}
 		}
+	}
+	
+	private void handleShowInfo() {
+		
+	}
+	
+	private void handleChangeInfo() {
+		JPanel info = new JPanel();
+		info.setLayout(new GridBagLayout());
+		
+		JTextField usernameField = new JTextField(10);
+		JTextField passwordField = new JTextField(10);
+		
+		JLabel usernameLabel = new JLabel("Username: ");
+		usernameLabel.setLabelFor(usernameField);
+		
+		JLabel passwordLabel = new JLabel("Password: ");
+		passwordLabel.setLabelFor(passwordField);
+		
+		GridBagConstraints c = new GridBagConstraints();
+		c.anchor = GridBagConstraints.BASELINE_LEADING;
+		c.insets = new Insets(4,4,4,4);
+		
+		c.gridwidth = 1;
+		
+		
+		if (signedIn instanceof Musician) {
+			
+		}
+		
+		int result = JOptionPane.showConfirmDialog(null, info, "Edit Information", JOptionPane.PLAIN_MESSAGE);
+	
 	}
 }
