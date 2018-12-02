0b26a9dfe636b35a775784fa785f5ce88a4c2497
Miranda <mirandahampton@email.arizona.edu>
2018-12-02 15:17:58 -0700

    Auto stash before merge of "master" and "origin/master"

--- src/gui/MainHub.java
+++ src/gui/MainHub.java
@@ -8,6 +8,7 @@ import javax.swing.*;
 
 import program.*;
 import users.*;
+import posts.*;
 
 public class MainHub extends JFrame{
 	private Hub session;
@@ -22,6 +23,10 @@ public class MainHub extends JFrame{
 	private JMenu lookup;
 	private JMenuItem exit;
 	private JMenuItem memberLookup;
+	private JPanel feed;
+	private ArrayList<JLabel> user;
+	private ArrayList<JTextArea> content;
+	private ArrayList<JPanel> likes;
 	private JMenu posts;
 	
 	private JTextField lookupName;
@@ -75,8 +80,46 @@ public class MainHub extends JFrame{
 		memberLookup = new JMenuItem("Member Lookup");
 		memberLookup.addActionListener(new MenuListener());
 		lookup.add(memberLookup);
+		feed = new JPanel();
+		user = new ArrayList<JLabel>();
+		content = new ArrayList<JTextArea>();
+		likes = new ArrayList<JPanel>();
 		
 		exit.addActionListener(new MenuListener());
+		for(int i=0; i<10; i++) {
+			Post post = new StatusUpdate();
+			post.setContent("test" + Integer.toString(i));
+			Musician m = new Musician();
+			m.setRealName(Integer.toString(i));
+			post.setOwner(m);
+			String numLikes = Integer.toString(post.getLikes());
+			JLabel labelUser = new JLabel(post.getOwner().getRealName());
+			JTextArea contentArea = new JTextArea(post.getContent());
+			JPanel likePanel = new JPanel();
+			JLabel likeLabel = new JLabel(numLikes);
+			JCheckBox click = new JCheckBox();
+			user.add(labelUser);
+			content.add(contentArea);
+			likePanel.add(click);
+			likePanel.add(likeLabel);
+			likePanel.add(new JSeparator());
+			likes.add(likePanel);
+			
+		}
+		
+		for(int i = 0; i<user.size(); i++) {
+			feed.add(user.get(i));
+			feed.add(content.get(i));
+			feed.add(likes.get(i));
+			feed.add(likes.get(i));
+			//feed.add(new JSeparator());
+		}
+		GridLayout gl = new GridLayout(0,1);
+		feed.setLayout(gl);
+		add(feed);
+		
+		
+		
 		signOut.add(exit);
 		
 		menuBar.add(profileOptions);
